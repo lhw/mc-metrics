cd524310c553c4d8834db2b486a546c3dd03d3af
Lennart Weller <lhw@ring0.de>
2019-09-07 19:16:36 +0200

    globalize config vars
    Signed-off-by: Lennart Weller <lhw@ring0.de>

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from prom import Exporter
 
 from prometheus_async.aio.web import start_http_server
 
+# Configuration parameters
+EXPORTER_PORT = int(os.getenv("MINECRAFT_EXPORTER_PORT", 8080))
+EXPORTER_INTERVAL = int(os.getenv("MINECRAFT_EXPORTER_INTERVAL", 60))
+
+RCON_HOST = os.getenv("MINECRAFT_RCON_HOST", "127.0.0.1")
+RCON_PORT = int(os.getenv("MINECRAFT_RCON_PORT", 27015))
+RCON_PASSWORD = os.getenv("MINECRAFT_RCON_PASSWORD", "minecraft")
+RCON_TIMEOUT = int(os.getenv("MINECRAFT_RCON_TIMEOUT", 5))
+
+IS_FORGE = bool(os.getenv("MINECRAFT_FORGE", False))
+
 if __name__ == '__main__':
-    # Configuration parameters
-    EXPORTER_PORT = int(os.getenv("MINECRAFT_EXPORTER_PORT", 8080))
-    EXPORTER_INTERVAL = int(os.getenv("MINECRAFT_EXPORTER_INTERVAL", 60))
-
-    RCON_HOST = os.getenv("MINECRAFT_RCON_HOST", "127.0.0.1")
-    RCON_PORT = int(os.getenv("MINECRAFT_RCON_PORT", 27015))
-    RCON_PASSWORD = os.getenv("MINECRAFT_RCON_PASSWORD", "minecraft")
-    RCON_TIMEOUT = int(os.getenv("MINECRAFT_RCON_TIMEOUT", 5))
-
-    IS_FORGE = bool(os.getenv("MINECRAFT_FORGE", False))
-    
     print("Starting Prometheus Exporter on Port {}".format(EXPORTER_PORT))
     asyncio.ensure_future(start_http_server(port=EXPORTER_PORT))
     exp = Exporter(EXPORTER_INTERVAL, IS_FORGE)
